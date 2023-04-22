ca217bfa23e2862d2b7e238dbe4a581ec55fa494
karchkha <karchkha@gmail.com>
2023-04-22 13:23:45 -0700

    xxx

--- src/utilities/parameters.py
+++ src/utilities/parameters.py
@@ -51,10 +51,10 @@ This will automatically take care of creating and training networks on which the
 ex: ['magri'] magri depends on all other networks. All networks will be trained
 ex: ['realdiff'] realdiff depends on 'real'. Only 'real' and 'realdiff' will be trained
 '''
-models_to_train_1 = ['mag', 'magl', 'magr', 'real', 'imag', 'realmean', 'realstd', 'imagmean', 'imagstd'] #[] #['imag', 'imagmean', 'imagstd'] #[] #['magl', 'magr'] #
-models_to_train_2 = ['magri']
-models_to_train_3 = ['magfinal']
-models_to_train_4 = ['magtotal']
+models_to_train_1 = ['mag'] #, 'magl', 'magr', 'real', 'imag', 'realmean', 'realstd', 'imagmean', 'imagstd'] #[] #['imag', 'imagmean', 'imagstd'] #[] #['magl', 'magr'] #
+models_to_train_2 = [] #['magri']
+models_to_train_3 = [] #['magfinal']
+models_to_train_4 = [] #['magtotal']
 # uncomment the line below if you want to predict other models
 # models_to_predict = ['magl', 'maglmean', 'maglstd'] # [ 'magr', 'magrmean', 'magrstd'] #['mag', 'magri', 'magfinal', 'magtotal'] #
 
