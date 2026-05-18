05ac0686464bb476e0b79ebda082d2cb8d2077bb
karchkha <karchkha@gmail.com>
2026-05-18 20:17:56 +0200

    small changes to support cipic-corr-height

--- src/utilities/parameters.py
+++ src/utilities/parameters.py
@@ -14,7 +14,7 @@ num_valid_points = int(round(percent_valid_points*points_per_ring,1))
 test_seed = 0
 validation_seed = 100
 batch_size = 32
-dropout = 0.0
+dropout = 0.1
 remove_mean = False
 initial_lr = 0.001
 
